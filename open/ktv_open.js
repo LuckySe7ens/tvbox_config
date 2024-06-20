c81fd18b7ea7e3e5ea5a4bdabad534a752940ee2
leospring <564892076@qq.com>
2024-06-20 20:19:29 +0800

    Update ktv_open.js

--- open/ktv_open.js
+++ open/ktv_open.js
@@ -1,23 +1,27 @@
 import { Crypto, load, _ } from './lib/cat.js';
 
 let key = 'ktv';
-let HOST = 'https://vpsdn.leuse.top/searchmv';
+//let HOST = 'https://vpsdn.leuse.top/searchmv';
 let mktvUrl = 'http://txysong.mysoto.cc/songs/';
+const HOST = 'https://api.cloudflare.com/client/v4/accounts/1ecc4a947c5a518427141f4a68c86ea1/d1/database/4f1385ab-f952-404a-870a-e4cfef4bd9fd/query';
 let host = '';
 let siteKey = '';
 let siteType = 0;
 
 const MOBILE_UA = 'Mozilla/5.0 (Linux; Android 11; M2007J3SC Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045714 Mobile Safari/537.36';
+const headers = {
+    'User-Agent': MOBILE_UA,
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer LueNrycW-6jks7xBjPqX9mjFq2A2M5Kul6Ig3D8z',
+};
+async function request(reqUrl, params) {
 
-async function request(reqUrl, extHeader) {
-    let headers = {
-        'User-Agent': MOBILE_UA,
-        'Referer': host,
-    };
     const res = await req(reqUrl, {
-        method: 'get',
+        method: 'post',
+        data: params,
         headers: headers,
     });
+    //console.log('res', res.content)
     return res.content;
 }
 
@@ -52,13 +56,26 @@ async function category(tid, pg, filter, extend) {
     if (pg <= 0 || typeof (pg) == 'undefined') pg = 1;
     let url = HOST;
     let videos = [];
+    let params = [];
+    let sql = '';
+    const size = 30;
     if(tid == 1) {
-        url = url + `?table=singer&pg=${pg}`;
-        if(extend['region']) url = url + '&where=region_id&keywords=' + extend['region'];
-        if(extend['form']) url += '&where=form_id&keywords=' + extend['form'];
-        console.log(url);
-        let res = JSON.parse(await request(url));
-        videos = _.map(res, item => {
+        sql = 'select name, id from singer where 1=1';
+        if(extend['region']) {
+            params.push(extend['region']);
+            sql += ' and region_id = ?';
+        }
+        if(extend['form']) {
+            params.push(extend['form']);
+            sql += ' and form_id = ?';
+        }
+        sql += ` limit ${pg-1},20;`;
+        //url = url + `?table=singer&pg=${pg}`;
+        //if(extend['region']) url = url + '&where=region_id&keywords=' + extend['region'];
+        //if(extend['form']) url += '&where=form_id&keywords=' + extend['form'];
+        //console.log(sql, params);
+        let res = JSON.parse(await request(url, {params: params, sql: sql}));
+        videos = _.map(res.result[0].results, item => {
             return {
                 vod_id: item.name,
                 vod_name: item.name,
@@ -67,11 +84,21 @@ async function category(tid, pg, filter, extend) {
             }
         });
     } else if(tid == 2) {
-        url = url + `?table=song&pg=${pg}`;
-        if(extend['lan']) url = url + '&where=language_id&keywords=' + extend['lan'];
-        if(extend['type']) url += '&where=type_id&keywords=' + extend['type'];
-        let res = JSON.parse(await request(url));
-        videos = _.map(res, item => {
+        sql = 'select number, name from song where 1=1';
+        if(extend['lan']) {
+            params.push(extend['lan']);
+            sql += ' and language_id = ?';
+        }
+        if(extend['type']) {
+            params.push(extend['type']);
+            sql += ' and type_id = ?';
+        }
+        sql += ` limit ${pg-1},20;`;
+        //url = url + `?table=song&pg=${pg}`;
+        //if(extend['lan']) url = url + '&where=language_id&keywords=' + extend['lan'];
+        //if(extend['type']) url += '&where=type_id&keywords=' + extend['type'];
+        let res = JSON.parse(await request(url, {params:params, sql:sql}));
+        videos = _.map(res.result[0].results, item => {
             return {
                 vod_id: mktvUrl + item.number + '.mkv',
                 vod_name: item.name,
@@ -98,9 +125,11 @@ async function detail(id) {
     if (id.endsWith('.mkv')) {
         vod.vod_play_url = '播放$' + id;
     } else {
-        let url = HOST + '?table=song&where=singer_names&keywords=' + id + '&size=999';
-        let res = JSON.parse(await request(url));
-        vod.vod_play_url = (_.map(res, item => {
+        let params = [id];
+        let sql = 'select number,name from song where singer_names = ? limit 0,999';
+        //let url = HOST + '?table=song&where=singer_names&keywords=' + id + '&size=999';
+        let res = JSON.parse(await request(HOST, {params:params,sql:sql}));
+        vod.vod_play_url = (_.map(res.result[0].results, item => {
             return item.name + '$' + mktvUrl + item.number + '.mkv';
         })).join('#');
     }
@@ -117,8 +146,9 @@ async function play(flag, id, flags) {
 }
 
 async function search(wd, quick) {
-    let data = JSON.parse(await request(HOST + 'keywords=' + wd));
-    let videos = _.map(data, (it) => {
+    let sql = "select number,name from song where name like '%"+wd+"%' or single_names like '%"+wd+"%'";
+    let data = JSON.parse(await request(HOST,{sql:sql}));
+    let videos = _.map(data.result[0].results, (it) => {
         return {
             vod_id: mktvUrl + it.number + '.mkv',
             vod_name: it.name,
