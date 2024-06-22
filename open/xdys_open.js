22952da91cb510c591a15579e42f0832d7aa9345
leospring <564892076@qq.com>
2024-06-23 01:05:58 +0800

    Update xdys_open.js

--- open/xdys_open.js
+++ open/xdys_open.js
@@ -10,7 +10,6 @@ let siteKey = '';
 let siteType = 0;
 
 const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
-
 async function request(reqUrl) {
     let res = await req(reqUrl, {
         method: 'get',
