09155ecf805ba207dfd7d09ecc1884fa15d8b1f2
leospring <564892076@qq.com>
2024-10-17 02:21:23 +0800

    Update nongmin_open.js

--- open/nongmin_open.js
+++ open/nongmin_open.js
@@ -7,7 +7,7 @@
     let siteKey = '';
     let siteType = 0;
     let headers = {
-        //'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
         'Referer': siteUrl + '/'
     };
     let PARSE_URL = 'https://api.cnmcom.com/webcloud/relay.php';
